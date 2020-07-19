74e3b6ef8549f9cac079ce25f98ef12aa96b2fdd
Sachindra <sachindrafernando3@gmail.com>
2020-07-20 01:55:04 +0530

    building category section

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -11,7 +11,10 @@ class HomePage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-          Text('Categories', style: TextStyle(fontSize: 40),),
+          SizedBox(height: 10,),
+          Center(child: Text('Categories', style: TextStyle(fontSize: 
+          20, fontWeight: FontWeight.bold),)),
+          SizedBox(height: 10,),
           Category(),
           Text('Products', style: TextStyle(fontSize: 40),),
           //AllProducts(),
--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -11,12 +11,23 @@ CategoryCard(this.icon, this.name);
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 5,
+              color: Colors.blueGrey
+            )
+          ],
+        ),
+        width: 100,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
         child: Column(
         children: <Widget>[
           icon,
-          Text(name)
+          Text(name, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
           ],
           ),
       ),
