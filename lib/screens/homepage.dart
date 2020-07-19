788f9bf4c614899d02b4c1771b2c113002e18226
Sachindra <sachindrafernando3@gmail.com>
2020-07-19 21:45:37 +0530

    building home screen

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,10 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text('Flutter E Com'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Text('Categories', style: TextStyle(fontSize: 40),),
+          //Category(),
+          Text('Products', style: TextStyle(fontSize: 40),),
+          //AllProducts(),
+        ],
+      ),
     );
   }
 }
